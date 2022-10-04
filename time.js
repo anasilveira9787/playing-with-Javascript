2ea825751fb75bd5977472618d11926dfdf2576a
Lucas Exposito Rocha <107315686+LucasExpRocha@users.noreply.github.com>
2022-10-03 22:24:23 -0300

    Feito o timer

---
+++ time.js
@@ -0,0 +1,65 @@
+export function functionTime(){
+    const clickCircle = document.querySelector('#circle')
+
+    
+    const ballSeconds = document.querySelector('.ballSeconds')
+    const ballMinutes = document.querySelector('.ballMinutes');
+    const ballHours = document.querySelector('.ballHours');
+    
+    const textSeconds = document.querySelector('.textSeconds');
+    const textMinutes = document.querySelector('.textMinutes');
+    const textHours = document.querySelector('.textHours');
+
+    
+    const dateNow = new Date();
+    let minutes = dateNow.getMinutes();
+    let hours = dateNow.getHours();
+    let seconds = dateNow.getSeconds();
+    let stop;
+
+
+    function StylesByHours(){
+        let heightBall = (100/24)*(24-hours);
+        ballHours.style.height = `${heightBall}%`;
+        textHours.innerText = hours.toString().padStart(2, '0');
+        
+        if(hours == 23 && minutes >= 59 && seconds >= 59){
+            hours = 0;
+            StylesByHours()
+        }
+        
+    }
+
+    function StylesByMinutes(){
+        let heightBall = (100/60)*(60-minutes);
+        ballMinutes.style.height = `${heightBall}%`;
+        textMinutes.innerText = minutes.toString().padStart(2, '0');
+        
+        if(minutes >= 59 && seconds == 0) {
+            minutes = 0;
+            hours++;
+            StylesByMinutes()
+            StylesByHours()
+        }
+    }
+
+
+    function StylesBySeconds(){
+        stop = setInterval(function(){
+            let heightBall = (100/60)*(60-seconds);
+            ballSeconds.style.height = `${heightBall}%`;
+            if(seconds > 59) {
+                seconds = 0;
+                minutes++;
+                StylesByMinutes()
+            }
+            textSeconds.innerText = seconds.toString().padStart(2, '0');
+            seconds++
+        },1000)    
+    }
+
+
+    StylesBySeconds()
+    StylesByMinutes()
+    StylesByHours()
+}
