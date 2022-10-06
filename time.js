23bb1ecc70bb2956b0c8bc7e77b1d2f911d54ba4
Renan Rondon <renanrondon@live.com>
2022-10-06 13:12:33 -0300

    Correcting bugs

--- time.js
+++ time.js
@@ -1,65 +1,58 @@
-export function functionTime(){
-    const clickCircle = document.querySelector('#circle')
+export function functionTime() {
+    const clickCircle = document.querySelector('#circle');
 
-    
-    const ballSeconds = document.querySelector('.ballSeconds')
+    const ballSeconds = document.querySelector('.ballSeconds');
     const ballMinutes = document.querySelector('.ballMinutes');
     const ballHours = document.querySelector('.ballHours');
-    
+
     const textSeconds = document.querySelector('.textSeconds');
     const textMinutes = document.querySelector('.textMinutes');
     const textHours = document.querySelector('.textHours');
 
-    
     const dateNow = new Date();
-    let minutes = dateNow.getMinutes();
     let hours = dateNow.getHours();
+    let minutes = dateNow.getMinutes();
     let seconds = dateNow.getSeconds();
-    let stop;
 
-
-    function StylesByHours(){
-        let heightBall = (100/24)*(24-hours);
+    function StylesByHours() {
+        let heightBall = (100 / 24) * (24 - hours);
         ballHours.style.height = `${heightBall}%`;
         textHours.innerText = hours.toString().padStart(2, '0');
-        
-        if(hours == 23 && minutes >= 59 && seconds >= 59){
+
+        if (hours > 23) {
             hours = 0;
-            StylesByHours()
+            StylesByHours();
         }
-        
     }
 
-    function StylesByMinutes(){
-        let heightBall = (100/60)*(60-minutes);
+    function StylesByMinutes() {
+        let heightBall = (100 / 60) * (60 - minutes);
         ballMinutes.style.height = `${heightBall}%`;
         textMinutes.innerText = minutes.toString().padStart(2, '0');
-        
-        if(minutes >= 59 && seconds == 0) {
+
+        if (minutes >= 59 && seconds == 0) {
             minutes = 0;
             hours++;
-            StylesByMinutes()
-            StylesByHours()
+            StylesByMinutes();
+            StylesByHours();
         }
     }
 
-
-    function StylesBySeconds(){
-        stop = setInterval(function(){
-            let heightBall = (100/60)*(60-seconds);
+    function StylesBySeconds() {
+        setInterval(function () {
+            let heightBall = (100 / 60) * (60 - seconds);
             ballSeconds.style.height = `${heightBall}%`;
-            if(seconds > 59) {
+            if (seconds > 59) {
                 seconds = 0;
                 minutes++;
-                StylesByMinutes()
+                StylesByMinutes();
             }
             textSeconds.innerText = seconds.toString().padStart(2, '0');
-            seconds++
-        },1000)    
+            seconds++;
+        }, 1000);
     }
 
-
-    StylesBySeconds()
-    StylesByMinutes()
-    StylesByHours()
+    StylesBySeconds();
+    StylesByMinutes();
+    StylesByHours();
 }
